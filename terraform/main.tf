664b02a09c471a8cc73ece516ee1f8648d843d89
aradhakrishnan89 <arjun.radhakrishnan@tivo.com>
2019-10-17 00:59:37 +0530

    removed credentials

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
-resource "aws_key_pair" "nikhilsshkey" {
+resource "aws_key_pair" "userkey" {
     key_name = "ssh-key"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOsaNmi1splxA2a9RdJP20crtTanX/9LS3OcEBwKgFwKX9MPUX/QH6A8AGHzNA6JLHbOjjDJYWOQhlOj5lBFEJIwqJR77JB/WEB6FxmujR/kOz1r1f9QJdEiwpyU5G425Q+7Stqa9EQj5gOArfIa5a3q8h9eHm3LyIMu3o4hH+xlCho/i8zpP8y1Dk9cxC1UiutLDUEGIwdXlWkDpkRRiB3PAnh23UE4b2jiMD11eXcT/rFiI6QPXw8Oi6Sjkv2vs5h9CEiRwFKvCUowQMJOUceShYoVl2OGcPrKD7aSXadAMq+lP1A7lV0P/m5pPvy5HrxvxdxPHqPDNvsGxh0Zk7 npaa@sjc-1mpolava-lt.corporate.local"
+    public_key = "ssh-rsa AAAAxxxxxxxxxxxxxxxxxxxxx"
   
 }
 
